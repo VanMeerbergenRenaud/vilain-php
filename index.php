60fa18b2a762a37e137cf217df65dfdd29b93afe
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2024-02-06 14:17:01 +0100

    tailwind updates for <nav> and <ol> list

--- index.php
+++ index.php
@@ -53,27 +53,21 @@
 </head>
 <body>
     <div class="min-h-full">
-        <nav class="bg-gray-800">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div class="flex h-16 items-center justify-between">
-                    <div class="flex items-center">
-                        <div>
-                            <a href="/">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="24" height="16">
-                                    <path d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z" fill="#feabcd" />
-                                </svg>
-                            </a>
-                        </div>
-                        <div class="hidden md:block">
-                            <?php function urlIs($value) {
-                                return $_SERVER['REQUEST_URI'] === $value;
-                            } ?>
-                            <div class="ml-10 flex items-baseline space-x-4">
-                                <a href="/jiris" class="<?= urlIs('/jiris') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Jiris</a>
-                                <a href="/contacts" class="<?= urlIs('/contacts') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-                                <a href="/projets" class="<?= urlIs('/projets') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projet</a>
-                            </div>
-                        </div>
+        <nav class="bg-gray-800 px-12 py-1">
+            <div class="flex h-16 items-center justify-between">
+                <div class="flex items-center">
+                    <?php function urlIs($value) {
+                        return $_SERVER['REQUEST_URI'] === $value;
+                    } ?>
+                    <div class="flex items-center space-x-6">
+                        <a href="/" class="<?= urlIs('/') ? 'bg-gray-700 text-white' : 'text-gray-300' ?> hover:bg-gray-700 px-3 py-2 rounded-md font-medium">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="24" height="16">
+                                <path d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z" <?= urlIs('/') ? 'fill="whitesmoke"' : 'fill="#feabcd"' ?> />
+                            </svg>
+                        </a>
+                        <a href="/jiris" class="<?= urlIs('/jiris') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Jiris</a>
+                        <a href="/contacts" class="<?= urlIs('/contacts') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                        <a href="/projets" class="<?= urlIs('/projets') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projet</a>
                     </div>
                 </div>
             </div>
@@ -86,7 +80,7 @@
                 <section class="mb-4 mt-2">
                     <h2 class="font-semibold text-xl mb-4">Jiris à venir</h2>
                     <?php if (count($upcoming_jiris)): ?>
-                        <ol class="flex items-center gap-6">
+                        <ol class="flex gap-6">
                             <?php foreach ($upcoming_jiris as $jiri): ?>
                                 <li>
                                     <div class="min-w-96 py-6 px-12 pl-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
@@ -122,7 +116,7 @@
                 <section class="my-6 mt-12">
                     <h2 class="font-semibold text-xl mb-4">Jiris en cours</h2>
                     <?php if (count($current_jiris)): ?>
-                        <ol class="flex items-center gap-6">
+                        <ol class="flex gap-6">
                             <?php foreach ($current_jiris as $jiri): ?>
                                 <li>
                                     <div class="min-w-96 py-6 px-12 pl-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
@@ -158,7 +152,7 @@
                 <section class="my-6 mt-12">
                     <h2 class="font-semibold text-xl mb-4">Jiris passés</h2>
                     <?php if (count($passed_jiris)): ?>
-                        <ol class="flex items-center gap-6">
+                        <ol class="flex justify-end flex-row-reverse gap-6">
                             <?php foreach ($passed_jiris as $jiri): ?>
                                 <li>
                                     <div class="min-w-96 py-6 px-12 pl-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
