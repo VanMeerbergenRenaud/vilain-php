a16318165801c890b9e60432506c8f5acc47f9ac
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2024-02-12 17:00:36 +0100

    migration of data source to db
    use PDO class and add the logic of the connection of a database in a separate file

--- index.php
+++ index.php
@@ -6,41 +6,21 @@
         echo '</pre>';
     }
 
-    $jiris = [
-        ['id' => '6', 'name' => 'Projet web 2022', 'date' => '01-05-2022'],
-        ['id' => '1', 'name' => 'Projet web 2016', 'date' => '01-10-2016'],
-        ['id' => '84', 'name' => 'Projet web 2025', 'date' => '01-06-2025'],
-        ['id' => '987', 'name' => 'Projet web 2028', 'date' => '01-06-2028'],
-        ['id' => '78', 'name' => 'Projet web 2024', 'date' => '05-05-2024'],
-        ['id' => '64', 'name' => 'Projet web 2024', 'date' => '06-02-2024'],
-    ];
-
-    // Tri des jiris par dates
-    usort($jiris, function ($a, $b) {
-        return strtotime($a['date']) <=> strtotime($b['date']);
-    });
-
-    // dd($jiris);
-
-    $passed_jiris = [];
-    $current_jiris = [];
-    $upcoming_jiris = [];
-
-    $now = new DateTimeImmutable();
-
-    /* Le & devant $jiri -> &$jiri est un opérateur de référence. Il permet de modifier
-    directement l'élément du tableau dans la boucle, au lieu de créer une copie temporaire. */
-    foreach ($jiris as &$jiri) {
-        $jiri['date'] = DateTimeImmutable::createFromFormat('d-m-Y', $jiri['date']);
-
-        if ($jiri['date'] > $now) {
-            $upcoming_jiris[] = $jiri;
-        } elseif ($jiri['date'] < $now) {
-            $passed_jiris[] = $jiri;
-        } else {
-            $current_jiris[] = $jiri;
-        }
+    if (file_exists('database/database.php')) {
+        require 'database/database.php';
+    } else {
+        die('Problème de connection avec la db');
     }
+
+    $db = getPDO();
+
+    $statement = $db->query('SELECT * FROM jiris WHERE starting_at > current_timestamp');
+    $upcoming_jiris = $statement->fetchAll();
+
+    $statement = $db->query('SELECT * FROM jiris WHERE starting_at < current_timestamp');
+    $passed_jiris = $statement->fetchAll();
+
+    $current_jiris = [];
 ?>
 <!doctype>
 <html lang="fr-BE">
@@ -56,7 +36,8 @@
         <nav class="bg-gray-800 px-12 py-1">
             <div class="flex h-16 items-center justify-between">
                 <div class="flex items-center">
-                    <?php function urlIs($value) {
+                    <?php function urlIs($value): bool
+                    {
                         return $_SERVER['REQUEST_URI'] === $value;
                     } ?>
                     <div class="flex items-center space-x-6">
@@ -65,9 +46,12 @@
                                 <path d="M320 32c-8.1 0-16.1 1.4-23.7 4.1L15.8 137.4C6.3 140.9 0 149.9 0 160s6.3 19.1 15.8 22.6l57.9 20.9C57.3 229.3 48 259.8 48 291.9v28.1c0 28.4-10.8 57.7-22.3 80.8c-6.5 13-13.9 25.8-22.5 37.6C0 442.7-.9 448.3 .9 453.4s6 8.9 11.2 10.2l64 16c4.2 1.1 8.7 .3 12.4-2s6.3-6.1 7.1-10.4c8.6-42.8 4.3-81.2-2.1-108.7C90.3 344.3 86 329.8 80 316.5V291.9c0-30.2 10.2-58.7 27.9-81.5c12.9-15.5 29.6-28 49.2-35.7l157-61.7c8.2-3.2 17.5 .8 20.7 9s-.8 17.5-9 20.7l-157 61.7c-12.4 4.9-23.3 12.4-32.2 21.6l159.6 57.6c7.6 2.7 15.6 4.1 23.7 4.1s16.1-1.4 23.7-4.1L624.2 182.6c9.5-3.4 15.8-12.5 15.8-22.6s-6.3-19.1-15.8-22.6L343.7 36.1C336.1 33.4 328.1 32 320 32zM128 408c0 35.3 86 72 192 72s192-36.7 192-72L496.7 262.6 354.5 314c-11.1 4-22.8 6-34.5 6s-23.5-2-34.5-6L143.3 262.6 128 408z" <?= urlIs('/') ? 'fill="whitesmoke"' : 'fill="#feabcd"' ?> />
                             </svg>
                         </a>
-                        <a href="/jiris" class="<?= urlIs('/jiris') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Jiris</a>
-                        <a href="/contacts" class="<?= urlIs('/contacts') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-                        <a href="/projets" class="<?= urlIs('/projets') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projet</a>
+                        <a href="/jiris"
+                           class="<?= urlIs('/jiris') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Jiris</a>
+                        <a href="/contacts"
+                           class="<?= urlIs('/contacts') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contacts</a>
+                        <a href="/projets"
+                           class="<?= urlIs('/projets') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projets</a>
                     </div>
                 </div>
             </div>
@@ -84,11 +68,11 @@
                             <?php foreach ($upcoming_jiris as $jiri): ?>
                                 <li>
                                     <div class="min-w-96 py-6 px-12 pl-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-                                        <h3 class="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white"><?= $jiri['name'] ?></h3>
+                                        <h3 class="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white"><?= $jiri->name ?></h3>
                                         <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">
                                             <span>Date de commencement&nbsp;:</span>
-                                            <time datetime="<?= $jiri['date']->format('Y-m-d H:i:s'); ?>">
-                                                <?= $jiri['date']->format('j F Y'); ?>
+                                            <time datetime="<?= $jiri->starting_at ?>">
+                                                <?= $jiri->starting_at ?>
                                             </time>
                                         </p>
                                         <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">
@@ -99,7 +83,7 @@
                                             <span>Projets&nbsp;:</span>
                                             <span class="text-gray-500">vide</span>
                                         </p>
-                                        <a href="/jiris/<?= $jiri['id'] ?>" class="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        <a href="/jiris/<?= $jiri->id ?>" class="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             Voir le jiri
                                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -120,11 +104,11 @@
                             <?php foreach ($current_jiris as $jiri): ?>
                                 <li>
                                     <div class="min-w-96 py-6 px-12 pl-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-                                        <h3 class="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white"><?= $jiri['name'] ?></h3>
+                                        <h3 class="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white"><?= $jiri->name ?></h3>
                                         <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">
                                             <span>Date de commencement&nbsp;:</span>
-                                            <time datetime="<?= $jiri['date']->format('Y-m-d H:i:s'); ?>">
-                                                <?= $jiri['date']->format('j F Y'); ?>
+                                            <time datetime="<?= $jiri->starting_at ?>">
+                                                <?= $jiri->starting_at ?>
                                             </time>
                                         </p>
                                         <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">
@@ -135,7 +119,7 @@
                                             <span>Projets&nbsp;:</span>
                                             <span class="text-gray-500">vide</span>
                                         </p>
-                                        <a href="/jiris/<?= $jiri['id'] ?>" class="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        <a href="/jiris/<?= $jiri->id ?>" class="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             Voir le jiri
                                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -156,11 +140,11 @@
                             <?php foreach ($passed_jiris as $jiri): ?>
                                 <li>
                                     <div class="min-w-96 py-6 px-12 pl-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-                                        <h3 class="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white"><?= $jiri['name'] ?></h3>
+                                        <h3 class="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white"><?= $jiri->name ?></h3>
                                         <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">
                                             <span>Date de commencement&nbsp;:</span>
-                                            <time datetime="<?= $jiri['date']->format('Y-m-d H:i:s'); ?>">
-                                                <?= $jiri['date']->format('j F Y'); ?>
+                                            <time datetime="<?= $jiri->starting_at ?>">
+                                                <?= $jiri->starting_at ?>
                                             </time>
                                         </p>
                                         <p class="mb-4 font-normal text-gray-700 dark:text-gray-400">
@@ -171,7 +155,7 @@
                                             <span>Projets&nbsp;:</span>
                                             <span class="text-gray-500">vide</span>
                                         </p>
-                                        <a href="/jiris/<?= $jiri['id'] ?>" class="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        <a href="/jiris/<?= $jiri->id ?>" class="inline-flex items-center mt-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             Voir le jiri
                                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
